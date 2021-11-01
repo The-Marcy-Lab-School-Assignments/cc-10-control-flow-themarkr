708a0d729526630d0fcf2fb90a746184f8317d23
Mark Rodriguez <markrodriguezaa@gmail.com>
2021-11-01 13:39:01 +0000

    complete all four problems

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 //1
-function greatestOfTwo(){
-  
+function greatestOfTwo(num1, num2){
+  if (num1 > num2){
+      return num1;
+  }else{
+      return num2;
+  }
 }
+//console.log(greatestOfTwo(1, 14));
 
 //2
-function greatestOfThree(){
-  
+function greatestOfThree(num1, num2, num3){
+  if (num1 > num2 && num1 > num3){
+      return num1;
+  }else if (num2 > num1 && num2 > num3){
+      return num2;
+  }else{
+      return num3;
+  }
 }
+//console.log(greatestOfThree(-5, -2, -6));
 
 //3
-function greatestOfFour(){
-  
+function greatestOfFour(num1, num2, num3, num4){
+  if (num1 > num2 && num1 > num3 && num1 > num4){
+      return num1;
+  }else if (num2 > num1 && num2 > num3 && num2 > num4){
+      return num2;
+  }else if (num3 > num1 && num3 > num2 && num3 > num4){
+      return num3;
+  }else{
+      return num4;
+  }
 }
-
+//console.log(greatestOfFour(-5, -2, -6, 0))
 
 //4
-function leastOfFour(){
-  
+function leastOfFour(num1, num2, num3, num4){
+  if (num1 < num2 && num1 < num3 && num1 < num4){
+      return num1;
+  }else if (num2 < num1 && num2 < num3 && num2 < num4){
+      return num2;
+  }else if (num3 < num1 && num3 < num2 && num3 < num4){
+      return num3;
+  }else{
+      return num4;
+  }
 }
+//console.log(leastOfFour(10, 2, 6, 11));
